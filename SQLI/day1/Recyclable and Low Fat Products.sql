d70c0877c109025a2fc0e70074cd172ab61d0b3f
Amar Kumar <amar.mailsbox@gmail.com>
2022-08-15 00:09:47 +0530

    Update Daily SQL Question

---
+++ SQLI/day1/Recyclable and Low Fat Products.sql
@@ -0,0 +1,19 @@
+/*
+https://leetcode.com/problems/recyclable-and-low-fat-products/
+
+1757. Recyclable and Low Fat Products
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| product_id  | int     |
+| low_fats    | enum    |
+| recyclable  | enum    |
++-------------+---------+
+product_id is the primary key for this table.
+low_fats is an ENUM of type ('Y', 'N') where 'Y' means this product is low fat and 'N' means it is not.
+recyclable is an ENUM of types ('Y', 'N') where 'Y' means this product is recyclable and 'N' means it is not.
+*/
+
+--  Write your MySQL query statement below
+select product_id from Products where low_fats = 'Y' and recyclable = 'Y'
