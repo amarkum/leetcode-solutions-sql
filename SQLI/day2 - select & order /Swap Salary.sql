49c4d8e1a3e6d1173ecb1759d937b7c6556b16aa
Amar Kumar <amar.mailsbox@gmail.com>
2022-08-17 02:15:18 +0530

    Add Update and if condition

---
+++ SQLI/day2 - select & order /Swap Salary.sql
@@ -0,0 +1,31 @@
+/*
+https://leetcode.com/problems/swap-salary/
+
+627. Swap Salary
+
+SQL Schema
+Table: Salary
+
++-------------+----------+
+| Column Name | Type     |
++-------------+----------+
+| id          | int      |
+| name        | varchar  |
+| sex         | ENUM     |
+| salary      | int      |
++-------------+----------+
+id is the primary key for this table.
+The sex column is ENUM value of type ('m', 'f').
+The table contains information about an employee.
+
+
+Write an SQL query to swap all 'f' and 'm' values (i.e., change all 'f' values to 'm' and vice versa) with a single update statement and no intermediate temporary tables.
+
+Note that you must write a single update statement, do not write any select statement for this problem.
+*/
+
+
+--  Write your MySQL query statement below
+
+UPDATE salary
+SET sex = if(sex='f','m','f')
