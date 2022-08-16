8c41d4cc67ff59c5d4a1e6662f6bbfa48d564807
Amar Kumar <amar.mailsbox@gmail.com>
2022-08-16 23:56:09 +0530

    Add calculate special Bonus

---
+++ SQLI/day2 - select & order /Calculate Special Bonus.sql
@@ -0,0 +1,24 @@
+/*
+https://leetcode.com/problems/calculate-special-bonus/
+
+1873. Calculate Special Bonus
+
+SQL Schema
+Table: Employees
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| employee_id | int     |
+| name        | varchar |
+| salary      | int     |
++-------------+---------+
+employee_id is the primary key for this table.
+Each row of this table indicates the employee ID, employee name, and salary.
+
+Write an SQL query to calculate the bonus of each employee. The bonus of an employee is 100% of their salary if the ID of the employee is an odd number and the employee name does not start with the character 'M'. The bonus of an employee is 0 otherwise.
+
+Return the result table ordered by employee_id.
+*/
+
+--  Write your MySQL query statement below
