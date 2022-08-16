41b7beabd6357457ca237c38ce74e53eb48b6d37
Amar Kumar <amar.mailsbox@gmail.com>
2022-08-17 01:46:52 +0530

    If condition in MySQL

--- SQLI/day2 - select & order /Calculate Special Bonus.sql
+++ SQLI/day2 - select & order /Calculate Special Bonus.sql
@@ -22,3 +22,4 @@ Return the result table ordered by employee_id.
 */
 
 --  Write your MySQL query statement below
+select employee_id, if(employee_id %2 != 0 and name not like 'M%', salary, 0) as bonus from Employees
