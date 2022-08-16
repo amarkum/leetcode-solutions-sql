e972d0b63a588423336b106c0392deac12944a74
Amar Kumar <amar.mailsbox@gmail.com>
2022-08-17 02:29:20 +0530

    Update day3 SQL

--- SQLI/day2 - select & order /Calculate Special Bonus.sql
+++ SQLI/day2 - select & order /Calculate Special Bonus.sql
@@ -22,4 +22,4 @@ Return the result table ordered by employee_id.
 */
 
 --  Write your MySQL query statement below
-select employee_id, if(employee_id %2 != 0 and name not like 'M%', salary, 0) as bonus from Employees
+select employee_id, if(employee_id %2 != 0 and name not like 'M%', salary, 0) as bonus from Employees order by employee_id
---
+++ SQLI/day3 - string processing functions/Fix Names in a Table.sql
@@ -0,0 +1,21 @@
+/*
+1667. Fix Names in a Table
+
+SQL Schema
+Table: Users
+
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| user_id        | int     |
+| name           | varchar |
++----------------+---------+
+user_id is the primary key for this table.
+This table contains the ID and the name of the user. The name consists of only lowercase and uppercase characters.
+
+Write an SQL query to fix the names so that only the first character is uppercase and the rest are lowercase.
+
+Return the result table ordered by user_id.
+*/
+
+--  Write your MySQL query statement below
