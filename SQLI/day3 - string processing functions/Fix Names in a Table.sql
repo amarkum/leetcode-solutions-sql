0f723cf68cecad0d298336252b17c533bd2e2c2f
Amar Kumar <amar.mailsbox@gmail.com>
2022-08-18 01:51:17 +0530

    Add Initcap of column

--- SQLI/day3 - string processing functions/Fix Names in a Table.sql
+++ SQLI/day3 - string processing functions/Fix Names in a Table.sql
@@ -19,3 +19,4 @@ Return the result table ordered by user_id.
 */
 
 --  Write your MySQL query statement below
+SELECT user_id, CONCAT(UPPER(substring(name,1,1)), LOWER(substring(name, 2))) AS name FROM Users ORDER BY user_id;
