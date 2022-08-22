b0d559d5c1b7c2288047877de7ac358d556f9bae
Amar Kumar <amar.mailsbox@gmail.com>
2022-08-22 13:00:24 +0530

    Print data as tree

---
+++ SQLI/day4 - union & select/Tree Node.sql
@@ -0,0 +1,75 @@
+/*
+https://leetcode.com/problems/tree-node/
+
+608. Tree Node
+
+Table: Tree
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| p_id        | int  |
++-------------+------+
+id is the primary key column for this table.
+Each row of this table contains information about the id of a node and the id of its parent node in a tree.
+The given structure is always a valid tree.
+
+
+Each node in the tree can be one of three types:
+
+"Leaf": if the node is a leaf node.
+"Root": if the node is the root of the tree.
+"Inner": If the node is neither a leaf node nor a root node.
+Write an SQL query to report the type of each node in the tree.
+
+Return the result table ordered by id in ascending order.
+
+The query result format is in the following example.
+
+
+
+Example 1:
+
+
+Input:
+Tree table:
++----+------+
+| id | p_id |
++----+------+
+| 1  | null |
+| 2  | 1    |
+| 3  | 1    |
+| 4  | 2    |
+| 5  | 2    |
++----+------+
+Output:
++----+-------+
+| id | type  |
++----+-------+
+| 1  | Root  |
+| 2  | Inner |
+| 3  | Leaf  |
+| 4  | Leaf  |
+| 5  | Leaf  |
++----+-------+
+Explanation:
+Node 1 is the root node because its parent node is null and it has child nodes 2 and 3.
+Node 2 is an inner node because it has parent node 1 and child node 4 and 5.
+Nodes 3, 4, and 5 are leaf nodes because they have parent nodes and they do not have child nodes.
+*/
+
+--  Write your MySQL query statement below
+SELECT
+    id AS `Id`,
+    CASE
+        WHEN tree.id = (SELECT atree.id FROM tree atree WHERE atree.p_id IS NULL)
+          THEN 'Root'
+        WHEN tree.id IN (SELECT atree.p_id FROM tree atree)
+          THEN 'Inner'
+        ELSE 'Leaf'
+    END AS Type
+FROM
+    tree
+ORDER BY `Id`
+;
