9023db24fd2e3ca9fa2f037aa09f9f1410f84617
Amar Kumar <amar.mailsbox@gmail.com>
2022-08-22 12:49:21 +0530

    updatr SQL Queries

--- SQLI/day4 - union & select/Employees With Missing Information.sql
+++ SQLI/day4 - union & select/Employees With Missing Information.sql
@@ -38,3 +38,8 @@ The query result format is in the following example.
 */
 
 --  Write your MySQL query statement below
+select  employee_id from Employees
+where employee_id not in(select employee_id from Salaries)
+UNION
+select employee_id from  Salaries where employee_id not in(select employee_id from Employees)
+order by employee_id;
